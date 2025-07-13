247f49e257b1ae527fbd01bf43eb99b6f2f41cca
denbodHD <71848031+denbod@users.noreply.github.com>
2025-07-13 15:18:50 +0200

    MSP Arsenal Disabled

--- Missionframework/ui/mission_params.hpp
+++ Missionframework/ui/mission_params.hpp
@@ -189,7 +189,7 @@ class Params {
         title = $STR_PARAMS_MOBILEARSENAL;
         values[] = {0, 1};
         texts[] = {$STR_PARAMS_DISABLED, $STR_PARAMS_ENABLED};
-        default = 1;
+        default = 0;
     };
     class AttackedSectorRespawn {
         title = $STR_PARAMS_ATTACKEDSECTORRESPAWN;
